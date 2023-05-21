5711d8beb92bbcddc6ee62258e2b2b1d8815dcf1
diwuJake <131629444+diwuJake@users.noreply.github.com>
2023-05-21 21:58:54 +0800

    second commit

--- src/main/java/com/qiu/usercenter/UsercenterApplication.java
+++ src/main/java/com/qiu/usercenter/UsercenterApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UsercenterApplication {
 
     public static void main(String[] args) {
+        System.out.println("第二个版本");
         SpringApplication.run(UsercenterApplication.class, args);
     }
 
